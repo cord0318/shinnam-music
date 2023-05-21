78cb27675f675fe11204066ca2da9c503a3df5c0
cord0318 <cord0318@gmail.com>
2023-05-21 22:01:38 +0900

    package.json 설정

---
+++ build/node_modules/function-bind/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var implementation = require('./implementation');
+module.exports = Function.prototype.bind || implementation;
---
+++ build/node_modules/has/src/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var bind = require('function-bind');
+module.exports = bind.call(Function.call, Object.prototype.hasOwnProperty);
